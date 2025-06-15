5c1116e0d5acc4f0317dedd150a3c88e3e44cf00
Wilmer Miguel Castillo Tejada <wilmer_castillo@ucne.edu.do>
2025-06-15 19:11:11 -0400

    Creacion de TecnicosDto

---
+++ Tecnicos.Domain/DTO/TecnicosDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tecnicos.Domain.DTO
+{
+   public  class TecnicosDto
+    {
+        public int TecnicosId { get; set; }
+
+        public string? Nombres { get; set; }
+
+        public double Monto { get; set; }
+    }
+}

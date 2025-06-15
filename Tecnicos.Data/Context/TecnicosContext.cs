203475ca1ea8693cec470f8cdb68eead418f0ca1
Wilmer Miguel Castillo Tejada <wilmer_castillo@ucne.edu.do>
2025-06-15 19:10:41 -0400

    Algunas Creaciones en TecnicosData

---
+++ Tecnicos.Data/Context/TecnicosContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tecnicos.Data.Context
+{
+    public  class TecnicosContext : DbContext
+    {
+        public TecnicosContext(DbContextOptions<TecnicosContext> options) : base(options) { }
+
+        public DbSet<Models.Tecnico> Tecnico{ get; set; }
+    }
+}
---
+++ Tecnicos.Data/Models/Tecnico.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tecnicos.Data.Models
+{
+    public  class Tecnico
+    {
+        [Key]
+        public int TecnicoId { get; set; }
+
+        [Required(ErrorMessage = "Campo Obligatorio")]
+        [RegularExpression(@"^[a-zA-Z\s]+$",ErrorMessage ="Solo se Permiten Letras")]
+
+        public string? Nombres { get; set; }
+
+        [Required(ErrorMessage = "Campo Obligatorio")]
+        [RegularExpression(@"^\d+(\.\d{1,2})?$", ErrorMessage = "Solo se permiten numeros")]
+        public double Monto { get; set; }
+
+    }
+}

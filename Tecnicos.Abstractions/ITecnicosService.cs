5b81e63107cc3018f459892297ee6c48c2ce00be
Wilmer Miguel Castillo Tejada <wilmer_castillo@ucne.edu.do>
2025-06-15 19:10:00 -0400

    Creacion del ITecnicoService

---
+++ Tecnicos.Abstractions/ITecnicosService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using Tecnicos.Domain.DTO;
+
+namespace Tecnicos.Abstractions
+{
+    public interface ITecnicosService
+    {
+        Task<bool> Guardar(TecnicosDto tecnicos);
+        Task<bool> Eliminar (int tecnicosId);
+
+        Task<TecnicosDto> Buscar(int id);
+
+        Task<List<TecnicosDto>> Listar(Expression<Func<TecnicosDto, bool>> criterio);
+
+        Task<bool> ExisteTecnicos(int id, string nombres);
+    }
+}

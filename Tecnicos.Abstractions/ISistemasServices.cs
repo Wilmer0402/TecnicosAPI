69a9d633e5115c9cf142eba79af3c34b20e119c8
Wilmer Miguel Castillo Tejada <wilmer_castillo@ucne.edu.do>
2025-07-23 14:07:28 -0400

    Creacion de ISistemasServices

---
+++ Tecnicos.Abstractions/ISistemasServices.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using Tecnicos.Domain.DTO;
+
+namespace Tecnicos.Abstractions
+{
+    public interface ISistemasService
+    {
+        Task<bool> Guardar(SistemasDto cliente);
+        Task<bool> Eliminar(int sistemaId);
+        Task<SistemasDto> Buscar(int id);
+        Task<List<SistemasDto>> Listar(Expression<Func<SistemasDto, bool>> criterio);
+        Task<bool> ExisteSistema(int id, string nombre);
+
+    }
+}

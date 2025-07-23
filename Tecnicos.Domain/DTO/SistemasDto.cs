19c0edf9c0f8bc6ed897855fecc3b14ffa9ec0c3
Wilmer Miguel Castillo Tejada <wilmer_castillo@ucne.edu.do>
2025-07-23 14:05:10 -0400

    Creacion del SistemasDto

---
+++ Tecnicos.Domain/DTO/SistemasDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tecnicos.Domain.DTO
+{
+   public class SistemasDto
+    {
+        public int SistemaId { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+
+        public double Costo { get; set; }
+    }
+}
